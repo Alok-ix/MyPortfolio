02f7800bb773c27e23b9ddf994ddd8a1f40aba54
Alok-ix <alok.rajak6639@gmail.com>
2026-06-27 10:13:59 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    let filePath = req.url === '/' ? '/index.html' : req.url;
+    filePath = path.join(__dirname, filePath);
+
+    const ext = path.extname(filePath);
+    let contentType = 'text/html';
+    switch (ext) {
+        case '.css': contentType = 'text/css'; break;
+        case '.js': contentType = 'application/javascript'; break;
+        case '.json': contentType = 'application/json'; break;
+        case '.png': contentType = 'image/png'; break;
+        case '.jpg': case '.jpeg': contentType = 'image/jpeg'; break;
+        case '.svg': contentType = 'image/svg+xml'; break;
+        default: contentType = 'text/html';
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>404 - Not Found</h1><p>The requested resource does not exist.</p>');
+            } else {
+                res.writeHead(500);
+                res.end('500 - Internal Server Error');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(data);
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}/`);
+    console.log(`Press Ctrl+C to stop.`);
+});
